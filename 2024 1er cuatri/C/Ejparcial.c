6d43a1bdb9494eb24944e65adcf325b9737b60ab
Gonzo <100657040+Gonz4z4@users.noreply.github.com>
2024-04-29 16:56:46 -0300

    ejparcial

--- 2024 1er cuatri/C/Ejparcial.c
+++ 2024 1er cuatri/C/Ejparcial.c
@@ -10,27 +10,8 @@
     char fecha_expiracion[12]; //30-06-2024
     float valor_asegurado;
 };
-void cargarDatos(struct notebooks **inventario,int *cantidad){
-
-    (*cantidad)++;
-    *inventario=(struct notebooks*) realloc(*inventario,*cantidad * sizeof(struct notebooks)); 
-    //Aca le estoy pasando el puntero que apunta a la direccion de memoria de notebooks para que le asigne un espacio
-        printf("Ingrese los datos para la notebook %i\n",*cantidad);
-        printf("Codigo de maquina:\n");
-        scanf("%i", &((*inventario)[*cantidad-1].codigo_maquina));
-        //aca estoy apuntando con un scan a la direccion de memoria de *inventario, que es a que apunta a inventario
-        printf("Nombre:\n");
-        scanf("%s", (*inventario)[*cantidad-1].nombre_prestado);
-        printf("DNI:\n");
-        scanf("%ld", &((*inventario)[*cantidad-1].dni));
-        printf("fecha prestamo (dd-mm-aaaa):\n");
-        scanf("%s", (*inventario)[*cantidad-1].fecha_prestamo);
-        printf("fecha expiracion (dd-mm-aaaa):\n");
-        scanf("%s", (*inventario)[*cantidad-1].fecha_expiracion);
-        printf("valor asegurado:\n");
-        scanf("%f", &((*inventario)[*cantidad-1].valor_asegurado));
-}
-void imprimirDatos();
+void cargarDatos(struct notebooks **inventario,int *cantidad);
+void imprimirDatos(struct notebooks *inventario,int cantidad);
 void modificarDatos();
 void recargarDatos();
 void eliminarDatos();
@@ -59,6 +40,7 @@ int main() {
             } while(continuar!='n' && continuar!='N');
             break;
         case 2: //imprime los elementos del vector hermosamente
+                imprimirDatos(inventario, cantidad);
         
             break;
         case 3: //modificacion fecha con DNI
@@ -86,9 +68,40 @@ int main() {
             printf("ingrese un número válido\n");
             break;
         }
+        
     } while (select!=0);
     printf("Nos vemos!\n");
     
-
+    free(inventario); // Liberar memoria din�mica antes de salir
 return 0;
+}
+void cargarDatos(struct notebooks **inventario,int *cantidad){
+    (*cantidad)++;
+    *inventario=(struct notebooks*) realloc(*inventario,*cantidad * sizeof(struct notebooks)); 
+    //Aca le estoy pasando el puntero que apunta a la direccion de memoria de notebooks para que le asigne un espacio
+        printf("Ingrese los datos para la notebook %i\n",*cantidad);
+        printf("Codigo de maquina:\n");
+        scanf("%i", &((*inventario)[*cantidad-1].codigo_maquina));
+        //aca estoy apuntando con un scan a la direccion de memoria de *inventario, que es a que apunta a inventario
+        printf("Nombre:\n");
+        scanf("%s", (*inventario)[*cantidad-1].nombre_prestado);
+        printf("DNI:\n");
+        scanf("%ld", &((*inventario)[*cantidad-1].dni));
+        printf("fecha prestamo (dd-mm-aaaa):\n");
+        scanf("%s", (*inventario)[*cantidad-1].fecha_prestamo);
+        printf("fecha expiracion (dd-mm-aaaa):\n");
+        scanf("%s", (*inventario)[*cantidad-1].fecha_expiracion);
+        printf("valor asegurado:\n");
+        scanf("%f", &((*inventario)[*cantidad-1].valor_asegurado));
+}
+void imprimirDatos(struct notebooks *inventario,int cantidad){
+    for (int i = 0; i < cantidad; i++){
+        printf("NOTEBOOK %i\n",cantidad);
+        printf("Codigo maquina: %i\n",inventario[cantidad-1].codigo_maquina);
+        printf("nombre_prestado: %s\n",inventario[cantidad-1].nombre_prestado);
+        printf("dni %ld\n",inventario[cantidad-1].dni);
+        printf("fecha_prestamo %s\n",inventario[cantidad-1].fecha_prestamo);
+        printf("fecha_expiracion: %s\n",inventario[cantidad-1].fecha_expiracion);
+        printf("Valor asegurado: %f\n",inventario[cantidad-1].valor_asegurado);
+    }
 }
